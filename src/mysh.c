9b85a26917b2f3815a1ca941d7174e77b81af3b0
HyeonOh678 <bryanhasu@gmail.com>
2023-11-22 15:23:03 -0500

    added some code to mysh and tokenizer

--- src/mysh.c
+++ src/mysh.c
@@ -19,7 +19,8 @@ int main (int argc, char **argv) {
 	if (argc == 1) {
 		mode = INTERACTIVE;
 		printf("Starting mysh in interactive mode\n");
-	} else if (argc == 2) {
+	} 
+	else if (argc == 2) {
 		mode = BATCH;
 
 		if (access(argv[1], R_OK) == 0) {
@@ -27,15 +28,29 @@ int main (int argc, char **argv) {
 				fprintf(stderr, "ERROR: Invalid file, %s is not a shell script\n", argv[1]);
 				return EXIT_FAILURE;
 			}
-		} else {
+		} 
+		else {
 			fprintf(stderr, "ERROR: Invalid file: %s\n", strerror(errno));
 			return EXIT_FAILURE;
 		}
-	} else {
+
+		printf("in batch mode now\n");
+		//if file name is valid, we will pass it to our parser where it will open the file and read commands 
+		//it will read line by line 
+                struct stat st;
+                char* name = argv[0];
+
+                
+
+			
+
+	} 
+	else {
 		fprintf(stderr, "ERROR: Cannot initialize mysh with an argument count of %d\n", argc);
 		return EXIT_FAILURE;
 	}
-
+	
+	/*
 	while (1) {
 		printf("mysh> ");
 
@@ -55,6 +70,6 @@ int main (int argc, char **argv) {
 				// figure out what to do with the command
 			// else error and return
 	}
-
+	*/
 	return EXIT_SUCCESS;
 }
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,3 +6,47 @@
 #include <sys/stat.h>
 
 //this will parse the arguments given to mysh
+//first let's make parse for batch mode for now
+
+#define DEBUG 0
+#define BUFFLEN 1
+
+void parser(char* name)
+{
+	char buffer[BUFFLEN];
+
+	int position = 0;
+	int bytes = 0;
+	int commandLength = 0;
+	int fd;
+	int start;
+
+	char* cmd = NULL; //this will be the command that is parsed, for example echo 
+	fd = open(name, O_RDONLY);
+
+	if (fd < 0) {
+		perror(name);
+		exit(EXIT_FAILURE);
+	}
+
+	while((bytes = read(fd, buffer, BUFFLEN) > 0)
+	{
+		start = 0;
+		//we will parse the first command here
+		
+
+	}		
+			
+
+
+
+
+
+
+
+
+
+
+
+
+}
