ec30c6eb7f8a1624e27bf3431ad0a774681d3755
HyeonOh678 <bryanhasu@gmail.com>
2023-11-24 13:28:14 -0500

    Finished initial line parsing, might have to do more work later

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -11,42 +11,58 @@
 #define DEBUG 0
 #define BUFFLEN 1
 
-void parser(char* name)
+void tokenizer(char* line)
 {
-	char buffer[BUFFLEN];
-
-	int position = 0;
-	int bytes = 0;
-	int commandLength = 0;
-	int fd;
-	int start;
-
-	char* cmd = NULL; //this will be the command that is parsed, for example echo 
-	fd = open(name, O_RDONLY);
-
-	if (fd < 0) {
-		perror(name);
-		exit(EXIT_FAILURE);
-	}
-
-	while((bytes = read(fd, buffer, BUFFLEN) > 0)
-	{
-		start = 0;
-		//we will parse the first command here
-		
-
-	}		
+	int left = 0;
+        int right = 0;
+        int lineSize = strlen(line);
+        char* token = NULL;
+        int position = left;
+        
+	//pass through every character in line
+        while(right <= lineSize)
+        {	
+		//first edge case: if both pointers are at a whitespace, move both
+                if(line[right] == ' ' && line[left] == ' ')
+                {
+                    right++;
+                    left++;
+                }
+                else if(right == lineSize) //second edge case: if the right pointer is at the last index, move it
+                {
+                    right++;
+                }
+                else if(line[right] == ' ' || line[right] == '\0' && line[left] != ' ')
+                {
+                        //allocate space for token from right - left
+                        token = realloc(token, right - left);
+                        position = left;
+                        for(int i = 0; i < right - left; i++)
+                        {
+                                token[i] = line[position];
+                                position++;
+                        }
 			
+			//here we will append the token into the arraylist by calling the push function
 
+                        printf("token is: %s \n", token);
+                        
+                        right++;
+                        left = right;
+                        token = NULL;
+                        
 
+                }
+                else 
+                {
+                    right++;
+                    
+                }
+                       
+                
+                    
 
-
-
-
-
-
-
-
+        }
 
 
 }
