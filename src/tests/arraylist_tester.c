3553f44065a1c0af2224b1f04ed14944f3262431
dhvanipat <dhvanipat21@gmail.com>
2023-11-25 22:21:48 -0500

    update 2 for arraylist tester

--- src/tests/arraylist_tester.c
+++ src/tests/arraylist_tester.c
@@ -5,23 +5,22 @@
 void display_array_list(arraylist_t*);
 
 int main(int argc, char** argv) {
-    // initialized empty arraylist
-    arraylist_t* my_al = al_create(5);
-    display_array_list(my_al);
+    for (int i = 0; i < 2; i++) {
+        // initialized empty arraylist
+        arraylist_t* my_al = al_create(1);
+        display_array_list(my_al);
 
 
-    // after adding elements
-    al_push(my_al, "hello");
-    al_push(my_al, "world");
-    al_push(my_al, "a");
-    al_push(my_al, "b");
-    al_push(my_al, "c");
-    al_push(my_al, "d");
-    al_push(my_al, "e");
-    display_array_list(my_al);
+        // after adding elements
+        for (int j = 0; j < 26; j++) {
+            char x[] = {97 + j, 97 + j, 97 + j, 97 + j, 97 + j, '\0'};
+            al_push(my_al, (char*) x);
+        }
+        display_array_list(my_al);
 
-    // deconstructing
-    al_destroy(my_al);
+        // deconstructing
+        al_destroy(my_al);
+    }
 }
 
 void display_array_list (arraylist_t* arraylist) {
@@ -29,7 +28,7 @@ void display_array_list (arraylist_t* arraylist) {
     printf("Length: %u\n", arraylist->length);
     printf("Size: %u\n", arraylist->size);
     for (int i = 0; i < arraylist->length; i++) {
-        printf("a[%d]: %p\t\t *a[%d]: %s\n", i, arraylist->head[i], i, al_get(arraylist, i));
+        printf("a[%d]: \t%p\t\t *a[%d]: \t%s\n", i, arraylist->head[i], i, al_get(arraylist, i));
     }
     printf("---\n");
 }
