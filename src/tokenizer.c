b3d67dc9f76d9c9897e69008d28dfe88853f700d
HyeonOh678 <bryanhasu@gmail.com>
2023-11-28 03:17:10 -0500

    added new tokenizer

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,32 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include "arraylist.h"
 #include "tokenizer.h"
+#include <string.h>
 
-//this will parse the arguments given to mysh
-//first let's make parse for batch mode for now
-
-#define DEBUG 0
-#define BUFFLEN 1
 
 void tokenizer(arraylist_t* arr, char* line)
 {
-	
-	char* token;
-        char* line2 = strdup(line);
-       
-        token = strtok(line2, " ");
-        do
-        {
-	    al_push(arr, token);
-            printf("token: %s\n", token);
-        }
-        while (token = strtok(NULL, " "));
-	
-	free(line2);
+	int lineSize = strlen(line);
+	int i = 0;
+	int left = 0;
+	while(i <= lineSize)
+	{
+		if(line[i] == ' ' && line[left] == ' ')
+		{
+			i++;
+			left++;
+		}
+		else if(i == lineSize && line[left] == ' ' && line[i] == ' ')
+		{
+			i++;
+		}
+		else if(i == lineSize && line[left] != ' ' && line[i] != ' ')
+                {
+                        int size = i - left + 1;
+                        char* str = malloc(sizeof(char)* size);
+                        memcpy(str, &line[left], size);
+                        str[i - left] = '\0';
+			al_push(arr, str);
+                        //printf("token is: %s\n", str);
+                        i++;
+                        left = i;
+                        free(str);
+
+                }
+	       	else if(line[i] == ' ' && line[left] != ' ')
+		{
+			int size = i - left + 1;
+			char* str = malloc(sizeof(char)* size);
+		        memcpy(str, &line[left], size);
+			str[i - left] = '\0';
+			al_push(arr, str);
+			//printf("token is: %s\n", str);
+			i++;
+			left = i;
+			free(str);	
+		}
+		else if((line[i] == '<' || line[i] == '>' || line[i] == '|') && (line[left] != ' ' && line[left] != '<' && line[left] != '>' && line[left] != '|'))
+		{
+		    int size = i - left + 1;
+			char* str = malloc(sizeof(char)* size);
+		    memcpy(str, &line[left], size);
+			str[i - left] = '\0'; 
+			al_push(arr, str);
+		    free(str);
+		    //now we take the single char token
+		    int size2 = 2;
+            char* str2 = malloc(sizeof(char)* size2);
+            memcpy(str2, &line[i], size2);
+            str2[1] = '\0';
+            al_push(arr,str2);
+            i++;
+            left = i;
+            free(str2);
+		}
+		else if(line[i] == '<' || line[i] == '>' || line[i] == '|')
+		{
+		    int size = 2;
+		    char* str = malloc(sizeof(char) * size);
+		    memcpy(str, &line[i], size);
+		    str[1] = '\0';
+		    al_push(arr,str);
+		    i++;
+		    left = i;
+		    free(str);
+		}	
+		else
+		{
+			i++;
+		}
+		
+		
+	}
+
 
 }
